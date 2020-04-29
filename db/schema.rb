ab3d067a2a82b4dfdfaeed5ef532d280b2cf769a
FranGia95 <frangia95@gmail.com>
2020-04-29 12:58:26 +0200

    autorizzazioni e assoiazione content e user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_184123) do
+ActiveRecord::Schema.define(version: 2020_04_29_094957) do
 
   create_table "contents", force: :cascade do |t|
     t.string "titolo"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_184123) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
